26f3e439c2e220da6100d565610b1924869450ce
calvean <garnetsharara@gmail.com>
2023-04-02 17:14:03 +0200

    massive commit

--- app/ticket.py
+++ app/ticket.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 import mysql.connector
 import db
+import uuid
+from datetime import datetime
+from event import get_event
 
-# ticket.py 
+def create_order(event_id, user_id, price, num_tickets):
+    order_id = str(uuid.uuid4())
+    created_at = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+
+    # calculate the total cost of the order
+    total_cost = price * num_tickets
+
+    # create a new order in the database
+    conn = db.create_connection()
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO orders (id, event_id, user_id, num_tickets, price, total_cost, created_at) VALUES (%s, %s, %s, %s, %s, %s, %s)', (order_id, event_id, user_id, num_tickets, price, total_cost, created_at))
+    conn.commit()
+    conn.close()
+
+    return order_id
+
 
 def book_ticket(event_id, user_id, price, num_tickets):
     event = get_event(event_id)
@@ -16,20 +34,4 @@ def book_ticket(event_id, user_id, price, num_tickets):
     redirect_url = create_paypal_payment(price, order_id)
 
     return {'redirect_url': redirect_url}
-
-def create_order(event_id, user_id, price, num_tickets):
-    order_id = str(uuid.uuid4())
-    created_at = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-
-    # calculate the total cost of the order
-    total_cost = price * num_tickets
-
-    # create a new order in the database
-    conn = create_connection()
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO orders (id, event_id, user_id, num_tickets, price, total_cost, created_at) VALUES (%s, %s, %s, %s, %s, %s, %s)', (order_id, event_id, user_id, num_tickets, price, total_cost, created_at))
-    conn.commit()
-    conn.close()
-
-    return order_id
 
